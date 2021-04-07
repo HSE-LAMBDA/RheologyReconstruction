small fixes with gradient and loss multipliers

pipeline/dolfin_adjoint/elasticity_solver.py
@@ -638,9 +638,9 @@ class dolfin_adjoint_solver(elasticity_solver):
         grad_lambda, grad_mu, grad_rho = self._project_grads(grads)
         
         return loss, (
-            float(self.factor_lambda) * grad_lambda,
-            float(self.factor_mu) * grad_mu,
-            float(self.factor_rho) * grad_rho
+            grad_lambda,
+            grad_mu,
+            grad_rho
         )
 
 
@@ -756,7 +756,7 @@ class adjoint_equation_solver(elasticity_solver):
             # add to misfit functional
             for a, b in zip(preds, ground): j += 0.5 * np.sum(np.power(b - a, 2))
 
-        return j / float(self.time_steps), adj_source, disp_history
+        return j, adj_source, disp_history
 
 
     def _adjoint_forward(self, adj_source, save_callback=None):

pipeline/trainer.py
@@ -231,12 +231,12 @@ class BaseTrainer:
                     
                     if num_solver_type == 'adjoint_equation':
                         adj_solver = adjoint_equation_solver(
-                            preds_lambda, preds_mu, np.ones_like(preds_lambda), 
+                            preds_lambda, preds_mu, preds_rho, 
                             detector_coordinates
                         )
                     else:
                         adj_solver = dolfin_adjoint_solver(
-                            preds_lambda, preds_mu, np.ones_like(preds_lambda), 
+                            preds_lambda, preds_mu, preds_rho, 
                             detector_coordinates
                         )
 