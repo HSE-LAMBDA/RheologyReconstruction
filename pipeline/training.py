9a3fb7ce169c7e707119e9a5ada5c4c77de4cfd6
Leonid Gremyachikh <ligremyachikh@edu.hse.ru>
2021-04-19 19:41:32 +0300

    ref training

--- pipeline/training.py
+++ pipeline/training.py
@@ -5,41 +5,37 @@ import os
 
 from dataset import SeismogramDataset
 from neural_networks.segnet import SegNet_3Head
-from trainer import BaseTrainer
-from loggers import TensorboardLogger
 
-SEED = 0
+from utils import number_of_parameters
+from trainer import BaseTrainer
 
 
 def main():
-    random.seed(SEED)
-    np.random.seed(SEED)
-    torch.manual_seed(SEED)
-    torch.cuda.manual_seed(SEED)
+    seed = 0
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
     torch.backends.cudnn.deterministic = True
 
     device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
-    # device = 'cpu'
     ROOT_DIR = os.path.join(os.getcwd())
+
     dataset_path = os.path.join(ROOT_DIR, 'dolfin_adjoint', '2_subdomains')
     train_dataset = SeismogramDataset(dataset_path)
+
     model = SegNet_3Head()
-    # detector_coords = [(np.array([c, 2000.])) for c in np.linspace(0., 2000., 128)]  # TODO: research
-    detector_coords = [(np.array([c, 20.])) for c in np.linspace(0., 20., 128)]  # TODO: research
-    logger = TensorboardLogger()
+    number_of_parameters(model)
 
-    # TODO: test with nonempty logger
-    t = BaseTrainer(
-        model=model,
-        device=device,
-        train_dataset=train_dataset,
-        optimizer_type=torch.optim.Adam,
-        optimizer_params={'lr': 1e-3},
-        snapshot_interval=250,
-        logger=logger,
-    )
+    detector_coords = [(np.array([c, 2000.])) for c in np.linspace(0., 2000., 128)]
 
-    # TODO: test if everything is allright with batch_size > 1
+    t = BaseTrainer(model,
+                    device,
+                    train_dataset,
+                    optimizer_type=torch.optim.Adam,
+                    optimizer_params={'lr': 1e-3},
+                    snapshot_interval=10)
+
     t.train(detector_coords, batch_size=1, epochs=100, num_solver_type='dolfin_adjoint')
 
 
